7f96f6964da74e0fb5e4f7fa8e59dbaa5e4c8391
Konstantinos <ktheofilis@gmail.com>
2022-09-09 16:25:37 +0300

    Minor fix

--- server/routes/bonus.js
+++ server/routes/bonus.js
@@ -25,49 +25,53 @@ router.patch('/api/recommend/:userId', getUserById, async(req,res) => {
         const msBetweenDates = Math.abs(timestamp.getTime() - timestampNow.getTime());
         const hoursBetweenDates = msBetweenDates / (60 * 60 * 1000);
 
-        //Checks if 24 hours has passed
-        if (hoursBetweenDates < 24) {
-            // console.log("24hr hasn't pass yet")
-            // console.log(dbmatrix.matrix[userid-1])
+        //If the 24hrs hasn't pass or the matrix is null then run the algorithm.
+        if (hoursBetweenDates >= 24 || dbmatrix.matrix.length === 0){
+
+            const users = await getUsers()
+            users.sort((a,b)=>(a.userId > b.userId) ? 1 : -1)
+
+            array = []
+            var i = 0
+            for (const user of users){
+                var columns = []
+                var likedItems = user.saved
+                var recentItems = user.recent
+                var boughtItems = user.shoppingList
+                for (var j = 1; j < 21; ++j){
+                    if(likedItems.includes(j) && boughtItems.includes(j)){
+                        columns[j-1] = 4
+                    }
+                    else if(likedItems.includes(j)){
+                        columns[j-1] = 2
+                    }
+                    else if(boughtItems.includes(j)){
+                        columns[j-1] = 3
+                    }
+                    else if(recentItems.includes(j)){
+                        columns[j-1] = 1
+                    }
+                    else{
+                        columns[j-1] = 0
+                    }
+                }
+                array[i] = columns
+                i++
+            }
+
+            ratings = matrix.matrixFactorization(array)
+            dbmatrix.matrix = ratings
+            dbmatrix.timestamp = timestampNow
+            const updatedMatrix = await dbmatrix.save()
+
+            //Translate the array of ratings to array of ids and return it here
+            return ratings[userid-1]
+        }
+        else{
+            //else return the saved row that is for the spesific user
+            //Translate the array of ratings to array of ids and return it here
             return dbmatrix.matrix[userid-1]
         }
-        
-        const users = await getUsers()
-        users.sort((a,b)=>(a.userId > b.userId) ? 1 : -1)
-
-        array = []
-        var i = 0
-        for (const user of users){
-            var columns = []
-            var likedItems = user.saved
-            var recentItems = user.recent
-            var boughtItems = user.shoppingList
-            for (var j = 1; j < 21; ++j){
-                if(likedItems.includes(j) && boughtItems.includes(j)){
-                    columns[j-1] = 4
-                }
-                else if(likedItems.includes(j)){
-                    columns[j-1] = 2
-                }
-                else if(boughtItems.includes(j)){
-                    columns[j-1] = 3
-                }
-                else if(recentItems.includes(j)){
-                    columns[j-1] = 1
-                }
-                else{
-                    columns[j-1] = 0
-                }
-            }
-            array[i] = columns
-            i++
-        }
-
-        ratings = matrix.matrixFactorization(array)
-        dbmatrix.matrix = ratings
-        dbmatrix.timestamp = timestampNow
-        const updatedMatrix = await dbmatrix.save()
-        return ratings[userid-1]
     }
     catch(err){
         res.status(500).json({message : err.message})
